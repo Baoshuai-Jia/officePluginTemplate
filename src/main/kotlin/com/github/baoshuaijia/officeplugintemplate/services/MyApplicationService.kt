87c37add8733d60979b98add5ce2f25ef1583526
GitHub Action <action@github.com>
2022-06-08 09:29:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/baoshuaijia/officeplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.baoshuaijia.officeplugintemplate.services
+
+import com.github.baoshuaijia.officeplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/baoshuaijia/officeplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/baoshuaijia/officeplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.baoshuaijia.officeplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.baoshuaijia.officeplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
